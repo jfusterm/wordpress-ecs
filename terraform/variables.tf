dd9c1fedb4479f229c1ccd63eb1eb35ecbb4eabf
Joan Fuster <joan.fuster@gmail.com>
2016-04-09 07:43:57 +0200

    Updated AMIs with 2016.03 version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -40,14 +40,14 @@ variable "ecs_cluster_name" {
 variable "amis" {
   description = "ECS Container Instances AMIs"
   default = {
-    ap-northeast-1 = "ami-b3afa2dd"
-    ap-southeast-1 = "ami-0cb0786f"
-    ap-southeast-2 = "ami-cf6342ac"
-    eu-central-1   = "ami-341efb5b"
-    eu-west-1      = "ami-77ab1504"
-    us-east-1      = "ami-33b48a59"
-    us-west-1      = "ami-26f78746"
-    us-west-2      = "ami-65866a05"
+    ap-northeast-1 = "ami-7e4a5b10"
+    ap-southeast-1 = "ami-be63a9dd"
+    ap-southeast-2 = "ami-b8cbe8db"
+    eu-central-1   = "ami-9aeb0af5"
+    eu-west-1      = "ami-9c9819ef"
+    us-east-1      = "ami-67a3a90d"
+    us-west-1      = "ami-b7d5a8d7"
+    us-west-2      = "ami-c7a451a7"
   }
 }
 

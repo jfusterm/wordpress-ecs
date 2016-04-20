f3225e0ad16b91eac3979c7c49a82b9116fe87ff
Joan Fuster <joan.fuster@gmail.com>
2016-04-20 06:43:43 +0200

    Added EC2 instances names

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,6 +10,9 @@ resource "aws_instance" "ecs-instance01" {
     iam_instance_profile        = "ecsInstanceRole"
     security_groups             = ["${aws_security_group.wp-ecs-sg-tf.id}"]
     user_data                   = "#!/bin/bash\necho ECS_CLUSTER=${aws_ecs_cluster.default.name} > /etc/ecs/ecs.config"
+    tags {
+      Name = "ecs-instance01"
+    }
 }
 
 resource "aws_instance" "ecs-instance02" {
@@ -22,4 +25,7 @@ resource "aws_instance" "ecs-instance02" {
     iam_instance_profile        = "ecsInstanceRole"
     security_groups             = ["${aws_security_group.wp-ecs-sg-tf.id}"]
     user_data                   = "#!/bin/bash\necho ECS_CLUSTER=${aws_ecs_cluster.default.name} > /etc/ecs/ecs.config"
+    tags {
+      Name = "ecs-instance02"
+    }
 }
